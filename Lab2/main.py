92caa3a3a0a3f1da55847d0825b0abe87d315ff3
DianaD <diana_draghiciu@yahoo.com>
2022-10-23 12:05:30 +0300

    Documentation to methods added

--- Lab2/main.py
+++ Lab2/main.py
@@ -7,12 +7,22 @@ class ST:
         self.hashNr = 7
 
     def hashFunction(self, symbol):
+        """
+        Returns the hash for the hashtable for the given symbol
+        :param symbol: represents the identifier's symbol
+        :return: the hash
+        """
         s = 0
         for elem in symbol:
             s += ord(elem)
         return s % self.hashNr
 
     def checkIfSymbolInST(self, symbol):
+        """
+        Checks if the given identifier's symbol already exists in the symbol table
+        :param symbol: represents the identifier's symbol
+        :return: True if the symbol is found, False otherwise
+        """
         poz = self.hashFunction(symbol)
         for elem in self.table[poz]:
             if elem[0] == symbol:
@@ -20,6 +30,12 @@ class ST:
         return False
 
     def addToST(self, symbol, value):
+        """
+        Adds an identifier to the symbol table if it's symbol isn't already declared
+        :param symbol: represents the identifier's symbol
+        :param value: represents the identifier's value
+        :return: -
+        """
         poz = self.hashFunction(symbol)
         if poz not in self.table.keys():
             self.table[poz] = []
@@ -28,6 +44,11 @@ class ST:
             self.table[poz].append((symbol, value))
 
     def getValue(self, symbol):
+        """
+        Gets the value of the given identifier's symbol
+        :param symbol: represents the identifier's symbol
+        :return: the identifier's value
+        """
         poz = self.hashFunction(symbol)
         for (x, y) in self.table[poz]:
             if x == symbol:
